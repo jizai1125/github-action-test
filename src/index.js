b9ef11c5c0cd84bf1e3f28e15714d5846bd8d339
jizai <1414589221@qq.com>
2022-04-07 21:40:30 +0800

    m

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
-
 axios
-  .get("/test/")
-  .then((res) => {})
-  .catch((err) => {
-    console.log("axios error");
-  });
+.get("/test/")
+.then((res) => {})
+.catch((err) => {
+  console.log("axios error");
+});
+console.log('fa');
